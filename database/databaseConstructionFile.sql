fa8c99b64136b112bf1eb333b0abfc77a98505da
cporterbellarmine <cporter@bellarmine.edu>
2021-11-10 14:48:21 -0500

    absolute path fixes?? 1 hour CNP

--- database/databaseConstructionFile.sql
+++ database/databaseConstructionFile.sql
@@ -4,81 +4,19 @@ CREATE TABLE prerequisite (PrerequisiteLinkID INT, ClassID INT, PrerequisiteClas
 SHOW TABLES;
 DESCRIBE class;
 SHOW TABLES;
-LOAD DATA LOCAL INFILE '../Classes.txt' INTO TABLE class;
-LOAD DATA LOCAL INFILE '../Prerequisites.txt' INTO TABLE prerequisite;
+LOAD DATA LOCAL INFILE 'C:/Users/chris/software-project/database/Classes.txt' INTO TABLE class;
+LOAD DATA LOCAL INFILE 'C:/Users/chris/software-project/database/Prerequisites.txt' INTO TABLE prerequisite;
 
 SELECT * FROM class;
 
 SELECT * FROM prerequisite;
 
-SELECT prerequisite.CourseNumber AS Course, prerequisite.Prerequisite AS PrerequisiteNum
-FROM prerequisite, class
-LEFT JOIN class AS class1 
-ON prerequisite.CourseNumber = class1.ClassID
-LEFT JOIN class AS class2 
-ON prerequisite.Prerequisite=class2.ClassID
-HAVING prerequisite.Prerequisite = class.CourseNumber
-AND prerequisite.CourseNumber = class.CourseNumber;
-
-SELECT Prerequisite as classNumber, class.CourseNumber
-FROM prerequisite, class
-WHERE class.ClassID = prerequisite.Prerequisite;
-
-SELECT prerequisite.CourseNumber, prerequisite.Prerequisite, class.CourseNumber
-FROM prerequisite, class
-HAVING class.CourseNumber = prerequisite.CourseNumber;
-#LEFT JOIN class as c1
-	#ON prerequisite.CourseNumber = c1.ClassID
-#LEFT JOIN class as c2
-
-SELECT class.CourseNumber, prerequisite.PrerequisiteID
-FROM class, prerequisite
-WHERE class.classID = prerequisite.PrerequisiteID;
-
-SELECT Prerequisite
-FROM prerequisite;
-
-SELECT prerequisite.CourseID, prerequisite.PrerequisiteID
-FROM prerequisite;
-
-SELECT prerequisite.ClassID, class.CourseNumber, prerequisite.PrerequisiteClassID, class.CourseNumber AS PrerequisiteCourseNumber
-FROM class, prerequisite
-WHERE class.ClassID = prerequisite.ClassID;
-
-SELECT prerequisite.PrerequisiteClassID, class.CourseNumber, prerequisite.ClassID
-FROM class, prerequisite
-WHERE class.ClassID = prerequisite.PrerequisiteClassID;
-
-SELECT prerequisite.ClassID, class.CourseNumber
-FROM class, prerequisite
-WHERE class.ClassID = prerequisite.ClassID;
-
-SELECT prerequisite.PrerequisiteClassID, class.CourseNumber
-FROM class, prerequisite
-WHERE class.ClassID = prerequisite.PrerequisiteClassID;
-
-SELECT
-	(SELECT prerequisite.ClassID
-	FROM class, prerequisite
-	WHERE class.ClassID = prerequisite.ClassID),
-    (SELECT prerequisite.PrerequisiteClassID
-	FROM class, prerequisite
-	WHERE class.ClassID = prerequisite.PrerequisiteClassID)
-FROM class, prerequisite;
-    
-(SELECT prerequisite.ClassID, class.CourseNumber
-	FROM class, prerequisite
-	WHERE class.ClassID = prerequisite.ClassID)
-UNION (SELECT prerequisite.PrerequisiteClassID, class.CourseNumber
-	FROM class, prerequisite
-	WHERE class.ClassID = prerequisite.PrerequisiteClassID);
-
 #Prints the course and its prerequisite
 SELECT CourseNumber, PrereqCourseNumber
-FROM (SELECT prerequisite.PrerequsiteLinkID, prerequisite.ClassID, class.CourseNumber
+FROM (SELECT prerequisite.PrerequisiteLinkID, prerequisite.ClassID, class.CourseNumber
 	FROM class, prerequisite
 	WHERE class.ClassID = prerequisite.ClassID) as table1
-LEFT JOIN (SELECT prerequisite.PrerequsiteLinkID, prerequisite.PrerequisiteClassID, class.CourseNumber AS PrereqCourseNumber
+LEFT JOIN (SELECT prerequisite.PrerequisiteLinkID, prerequisite.PrerequisiteClassID, class.CourseNumber AS PrereqCourseNumber
 	FROM class, prerequisite
 	WHERE class.ClassID = prerequisite.PrerequisiteClassID) as table2
-ON table1.prerequsiteLinkID = table2.prerequsiteLinkID;
+ON table1.prerequisiteLinkID = table2.prerequisiteLinkID;
